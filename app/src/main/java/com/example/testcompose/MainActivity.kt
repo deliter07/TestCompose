7091d4377490ad88ed20a033a7b8ade9fd8500e7
GameDeliter <deliter.07@mail.ru>
2023-05-22 22:53:34 +0300

    end

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -3,44 +3,73 @@ package com.example.testcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.testcompose.ui.theme.TestComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TestComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            Column() {
+                ListItem(name = "Vlad Vlad", prof = "Noob")
+                ListItem(name = "Vlad Vlad", prof = "Noob")
+                ListItem(name = "Vlad Vlad", prof = "Noob")
+                ListItem(name = "Vlad Vlad", prof = "Noob")
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+private fun ListItem(name: String, prof: String){
+      Card(
+          modifier = Modifier
+              .fillMaxWidth()
+              .padding(10.dp),
+          shape = RoundedCornerShape(15.dp),
+          elevation = CardDefaults.cardElevation(5.dp)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TestComposeTheme {
-        Greeting("Android")
-    }
+          ) {
+          Box(){
+              Row(verticalAlignment = Alignment.CenterVertically){
+                  Image(
+                      painter = painterResource(id = R.drawable.avatar1),
+                      contentDescription = "name",
+                      contentScale = ContentScale.Crop,
+                      modifier = Modifier
+                          .padding(5.dp)
+                          .size(64.dp)
+                          .clip(CircleShape)
+
+                  )
+                  Column(modifier = Modifier.padding(start = 16.dp)) {
+                      Text(text = name)
+                      Text(text = prof)
+                  }
+
+              }
+          }
+      }
 }
